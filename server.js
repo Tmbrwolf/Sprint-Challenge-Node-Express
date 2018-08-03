2601a6a58d2fdc5f11cd1b39df1e27d05595a80f
Tmbrwolf <timberwolf.eric@gmail.com>
2018-08-03 17:27:16 -0500

    Finally got projects endpoint - again typo battle :(

--- server.js
+++ server.js
@@ -10,4 +10,14 @@ server.get("/", (req, res) => {
   res.send('Hello World');
 })
 
+server.get('/projects', (req, res) => {
+  projectModel.get()
+  .then(response => {
+    res.json(response)
+  })
+  .catch(() => {
+    res.status(500).json({ error: "The projects data could not be retrieved."})
+  })
+})
+
 server.listen(8000, () => console.log('API running on port 8000'));

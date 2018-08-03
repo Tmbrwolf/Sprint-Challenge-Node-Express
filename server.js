ab8bb2e360ea66b5813fba5c0f0bfd43368ee485
Tmbrwolf <timberwolf.eric@gmail.com>
2018-08-03 17:48:38 -0500

    Adding get, put, and delete (by ID) methods

--- server.js
+++ server.js
@@ -20,4 +20,50 @@ server.get('/projects', (req, res) => {
   })
 })
 
+server.get('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  projectModel.get(id)
+    .then(response => {
+      if (response.length < 1) {
+        res.status(404).json({ message: "The project with specified ID does not exist." })
+      } else {
+        res.json(response)
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ error: "The individual project ID fo info could not be retrieved." })
+    })
+});
+
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  // const { name, description } = req.body;
+  const projBody = req.body;
+  projectModel.update(id, projBody)
+    .then(response => {
+      if (response.length < 1) {
+        res.status(404).json({ message: 'The project with specified ID does not exist. Database not updated.'})
+      }
+      res.status(200).json(projBody);
+    })
+    .catch(() => {
+      res.status(500).json({ error: 'The project information could not be modified.'})
+    })
+});
+
+server.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  projectModel.remove(id)
+    .then((response) => {
+      if (response < 1) {
+        res.status(404).json({ message: 'The specified project does not exist - Not updated/removed' });
+      } else {
+        res.status(200).json(response);
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ error: 'The project was not deleted.' });
+    });
+});
+
 server.listen(8000, () => console.log('API running on port 8000'));
